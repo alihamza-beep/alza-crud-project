abad7b479f274ac7deeaea9dfbec95ebe7aa2e2a
Aleeza <aleezahamza2799@gmail.com>
2025-11-11 18:33:09 +0500

    Added traffic.js in database folder

---
+++ database/traffic.js
@@ -0,0 +1,5 @@
+export let students = [
+    { id: 1, name: "Aleeza", age: 20, course: "Web Development", custom1: "Option1", custom2: "Option2" },
+    { id: 2, name: "Myra", age: 19, course: "AI & ML", custom1: "Option2", custom2: "Option1" },
+    { id: 3, name: "Hamza", age: 21, course: "Data Science", custom1: "Option1", custom2: "Option2" }
+];
